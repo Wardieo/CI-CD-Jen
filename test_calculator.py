359d0c539525b56705b9a93cc95f093e5d938f35
Edward Mosquera <109460206+Wardieo@users.noreply.github.com>
2025-03-22 06:55:35 -0400

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -7,7 +7,7 @@ class TestCalculator(unittest.TestCase):
         self.calc = Calculator()
 
     def test_add(self):
-        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(10, 3), 5)
 
     def test_subtract(self):
         self.assertEqual(self.calc.subtract(10, 5), 5)
